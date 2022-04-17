32b4a70b3eb8d62d3bf0df1f508ae050d9aa5d55
dconthree <103386201+dconthree@users.noreply.github.com>
2022-04-17 10:31:33 -0700

    Website Merge (#4)
    * Updated home page buttons to show current mint status
    
    * More Files yays
    
    Co-authored-by: UDS <UDSBROOM@UDSAROOM.local>

--- www/connect_coming_soon.js
+++ www/connect_coming_soon.js
@@ -16,6 +16,8 @@ async function connectwallet() {
 	 $("div.connect_wallet_button").fadeIn(2000);
     }
 	$("div.coming_soon_class").fadeIn(2000);
+	$("div.coming_alert_button_class").show;
+	$("div.coming_alert_button_class").fadeTo(2000, 1.0)
     const accounts = await ethereum.request({ method: "eth_accounts" });
     console.log(accounts);
 	  
@@ -26,112 +28,6 @@ async function connectwallet() {
   }
 }
 
-
-// async function execute() {
-//  if (typeof window.ethereum !== "undefined") {
-//    contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-//    const abi = [
-//      {
-//        inputs: [
-//          {
-//            internalType: "string",
-//            name: "_name",
-//            type: "string",
-//          },
-//          {
-//            internalType: "uint256",
-//            name: "_favoriteNumber",
-//            type: "uint256",
-//          },
-//        ],
-//        name: "addPerson",
-//        outputs: [],
-//        stateMutability: "nonpayable",
-//        type: "function",
-//      },
-//      {
-//        inputs: [
-//          {
-//            internalType: "string",
-//            name: "",
-//            type: "string",
-//          },
-//        ],
-//        name: "nameToFavoriteNumber",
-//        outputs: [
-//          {
-//            internalType: "uint256",
-//            name: "",
-//            type: "uint256",
-//          },
-//        ],
-//        stateMutability: "view",
-//        type: "function",
-//      },
-//      {
-//        inputs: [
-//          {
-//            internalType: "uint256",
-//            name: "",
-//            type: "uint256",
-//          },
-//        ],
-//        name: "people",
-//        outputs: [
-//          {
-//            internalType: "uint256",
-//            name: "favoriteNumber",
-//            type: "uint256",
-//          },
-//          {
-//            internalType: "string",
-//            name: "name",
-//            type: "string",
-//          },
-//        ],
-//        stateMutability: "view",
-//        type: "function",
-//      },
-//      {
-//        inputs: [],
-//        name: "retrieve",
-//        outputs: [
-//          {
-//            internalType: "uint256",
-//            name: "",
-//            type: "uint256",
-//          },
-//        ],
-//        stateMutability: "view",
-//        type: "function",
-//      },
-//      {
-//        inputs: [
-//          {
-//            internalType: "uint256",
-//            name: "_favoriteNumber",
-//            type: "uint256",
-//          },
-//        ],
-//        name: "store",
-//        outputs: [],
-//        stateMutability: "nonpayable",
-//        type: "function",
-//      },
-//    ];
-//    const provider = new ethers.providers.Web3Provider(window.ethereum);
-//    const signer = provider.getSigner();
-//    const contract = new ethers.Contract(contractAddress, abi, signer);
-//    try {
-//      await contract.store(42);
-//    } catch (error) {
-//      console.log(error);
-//    }
-//  } else {
-//    document.getElementById("executeButton").innerHTML =
-//      "Please install MetaMask";
-//  }
-//}
 
 function art(){
 	
@@ -157,32 +53,26 @@ function edballoon(){
 	
 }
 
-	// connecting user wallet
 
-//function connectwallet(){
-//
-//	// insert metamask call here
-//	
-//alert("CONNECT FUNCTION GOES HERE")
-//	
-//	// move this part of the funciton to the metamask "Sucsessfully Connected" function
-//
-//$("div.connect_wallet_button").fadeOut(500);
-//$("div.coming_soon_class").fadeIn(2000);
-//
-//	
-//}
+function comingsoon_click(){
+	
+	alert("Presale Mint goes live at the end of April! Join our Discord for project updates :)")
+	
+}
 
-	// this function hides the execute box and reveals the connect button when the wallet is disconected.
 
-//function disconectwallet(){
-//	
-//	$("div.coming_soon_class").fadeOut(2000);
-//	$("div.connect_wallet_button").fadeIn(2000);
-//	
-//}
+function number_nfts_sold(){
+	
+	var mint_value=0;
+	return mint_value.toString() + "/5000";
+	
+}
+
+
 
 module.exports = {
+  comingsoon_click,
+  number_nfts_sold,
   art,
   perform,
   olderelder,
