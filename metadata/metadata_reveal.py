28ff2b2c75ff4e8679d382cd269d7d6caff22b0d
dchapmanfw <chapdani@gmail.com>
2022-04-27 17:07:47 -0400

    update reveal json

--- metadata/metadata_reveal.py
+++ metadata/metadata_reveal.py
@@ -5,12 +5,28 @@ NAME = "Run Ed."
 IMAGE_HTML = "https://runed.mypinata.cloud/ipfs/QmYRLGvnNUgdRNDgCemd31irarTpVG1SjKP5PfiQiFkmiY"
 FILE_NAME = "ED-B_FUCK-THE-METAVERSE"
 ATTRIBUTE_ED = ({"trait_type" : "Location", "value": "Unknown"}, {"trait_type" : "Puppet", "value": "Ed"})
+DESCRIPTION = """EXT. WEB3 -- LOCATION UNKNOWN
 
-for x in range(5001):
+     [Billy]   Hey Ed!
+
+     [Ed B]   What's Good?
+
+     [Billy]   What do you think of the Metaverse?
+
+     [Ed B]   ...
+
+     [Ed B]   ...
+
+     [Ed B]   ... Fuck the Metaverse!
+
+Run Ed. Reveal | 1080 x 1080 | H.264"""
+
+for x in range(5000):
     json_dict = {
     "name": f"{NAME} #{x}",
     "image": f"{IMAGE_HTML}/{FILE_NAME}.jpg",
     "animation_url": f"{IMAGE_HTML}/{FILE_NAME}.mp4",
+    "description": DESCRIPTION,
     "attributes": ATTRIBUTE_ED,
     }
     with open(f"{x}", "w") as f:
